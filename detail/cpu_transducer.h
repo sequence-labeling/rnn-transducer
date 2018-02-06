39311ac88f1da0500c449815853ba3a7400c14bf
dumiao <dumiao@innobuddy.com>
2018-02-06 17:09:56 +0800

    apply exp to two probs

--- detail/cpu_transducer.h
+++ detail/cpu_transducer.h
@@ -4,7 +4,27 @@
 #include <transducer_helper.h>
 template<typename ProbT>
 void
-CupTransducer<ProbT>::softmax(const ProbT* const act1,const ProbT* const act2,ProbT* probs,const int* const input_lengths,const int* const label_lengths)
+CpuTransducer<Prob>::exp_matrix(Prob* predict_probs,Prob* trans_probs,const int* const input_lengths,const int* const label_lengths)
+{
+ for (int mb = 0; mb < minibatch_; ++mb) {
+      for(int c=0;c<input_lenths[mb];++c)
+        {
+            for(int r = 0; r < alphabet_size_; ++r)
+             {
+                int col_offset = (mb + minibatch_ * c) * alphabet_size_;
+                trans_probs[r + col_offset] = std::exp(trans_probs[r+col_offset]);
+                if(c<label_lengths[mb])
+                {
+                predict_probs[r + col_offset] = std::exp(predict_probs[r+col_offset]);
+                }
+              }
+          }
+
+}
+}
+template<typename ProbT>
+void
+CpuTransducer<ProbT>::softmax(const ProbT* const act1,const ProbT* const act2,ProbT* probs,const int* const input_lengths,const int* const label_lengths)
 {
 for(int mb=0;mb<minibatch_;++mb)
 {
